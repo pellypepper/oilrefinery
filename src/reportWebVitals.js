70515cb962d2c51bd6bee19b4c16e22361f0647c
pelumi <ppeliance@gmail.com>
2025-01-13 22:37:52 +0000

    vital

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,13 @@
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
+
 const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    // Use on... functions instead of get...
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
