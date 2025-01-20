c284d405d0f74c5898a604816be38977f31dee16
pelumi <ppeliance@gmail.com>
2025-01-20 22:13:11 +0000

    n

---
+++ build/static/js/841.dcfb84d0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[841],{841:(e,n,s)=>{s.r(n),s.d(n,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=841.dcfb84d0.chunk.js.map
